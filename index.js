1cea4c3f51ab16d7f48c20d1d8c0d5fb5fe44e91
Hathra <marianb95@yahoo.com>
2020-04-07 16:18:23 +0300

    Commiting for test.

--- index.js
+++ index.js
@@ -3,17 +3,48 @@ const fetchData = async (searchTerm) => {
 		params: {
 			apikey: 'b28528bf',
 			s: searchTerm
-			//i: 'tt0848228'
 		}
 	});
 
-	console.log(response.data);
+	if (response.data.Error) {
+		return [];
+	}
+
+	return response.data.Search;
 };
 
+const root = document.querySelector('.suggestion');
+
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 const input = document.querySelector('input');
 
-const onInput = debounce((event) => {
-	fetchData(event.target.value);
+const onInput = debounce(async (event) => {
+	const movies = await fetchData(event.target.value);
+
+	resultsWrapper.innerHTML = '';
+	dropdown.classList.add('is-active');
+
+	for (let movie of movies) {
+		const option = document.createElement('a');
+
+		option.classList.add('dropdown-item');
+		option.innerHTML = `
+            <img src="${movie.Poster}"/>
+            ${movie.Title}
+        `;
+		resultsWrapper.appendChild(option);
+	}
 }, 500);
 
 input.addEventListener('input', onInput);
